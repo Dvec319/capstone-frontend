9cfffed13e15facec5ff82d51ce87c1341b3fe6c
mdelvecchio <mdelvecchio3119@gmail.com>
2023-10-07 11:50:16 -0400

    Set up router.js and pages

---
+++ src/pages/Create.js
@@ -0,0 +1,5 @@
+function Create(props) {
+	return <h1>This is the Create</h1>;
+}
+
+export default Create;
--- src/router.js
+++ src/router.js
@@ -1,12 +1,17 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import App from "./App";
+import Create from "./pages/Create";
+import Index from "./pages/Index";
+import Show from "./pages/Show";
 
 const router = createBrowserRouter (createRoutesFromElements(
     <Route path="/" element={<App/>}>
-       <Route path="" />
-       <Route path="ideas/:id" />
-       <Route path="ideas/create" />
+       <Route path="ideas" element={<Index/>}/>
+       <Route path="ideas/:id" element={<Show/>}/>
+       <Route path="ideas/create" element={<Create/>} />
        <Route path="update/:id" />
        <Route path="delete/:id" />
     </Route>
 ))
+
+export default router;
